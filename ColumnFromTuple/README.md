2743b8f3d2ce0ae52a5a932d92d3af7150e31029
Anthony Hein <anhein@princeton.edu>
2020-07-20 17:34:39 -0400

    Progress 7/20/2020 5:35 PM

---
+++ ColumnFromTuple/README.md
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/63004088/get-a-column-from-from-the-list-of-tuples
---
+++ ColumnFromTuple/soln.py
@@ -0,0 +1,5 @@
+lst = [('a','b','c'),
+       ('d','e','f')]
+
+col1 = [tple[1] for tple in lst]
+print(col1)
